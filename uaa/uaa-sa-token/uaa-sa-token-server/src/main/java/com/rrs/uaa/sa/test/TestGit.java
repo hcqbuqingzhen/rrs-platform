b43d11c93e4a39b555d83cb00bc2cca8373a9859
韩长庆 <1316662395@qq.com>
2022-10-31 23:31:33 +0800

    add： test Git

---
+++ uaa/uaa-sa-token/uaa-sa-token-server/src/main/java/com/rrs/uaa/sa/test/TestGit.java
@@ -0,0 +1,12 @@
+package com.rrs.uaa.sa.test;
+
+/**
+ * @Description:
+ * @author: hcq
+ * @date:2022/10/31 下午11:30
+ */
+public class TestGit {
+    public static void main(String[] args) {
+
+    }
+}
